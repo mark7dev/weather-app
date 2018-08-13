30f6be80d22c9d6d72d3ca8b69dd49246ec8ecec
mark7dev <mcamacho_ce7@live.com.mx>
2018-08-13 11:35:20 -0500

    version1.1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+     this.state = {
+      show: false
+    };
+  }
+
+   showInput = () => {
+    this.setState({
+      show: true
+    });
+  }
+
   render() {
     return (
       <div className='app'>
         <header className='app__header'>
-          <button className='app__add'>
+          <button onClick={ this.showInput } className='app__add'>
             <i className='fa fa-plus-circle' /> New city
           </button>
         </header>
@@ -15,7 +29,7 @@ class App extends Component {
           <aside className='app__aside'>
             <h1 className='app__title'>All countries</h1>
             <a href='#' className='app__country'>France</a>
-            <input type='text' placeholder='Location' className='app__input' />
+    { this.state.show && <input autoFocus type='text' placeholder='Location' className='app__input' /> }
           </aside>
           <section className='app__view'>Text</section>
         </div>
