dddbd2f51bfa812748ebeb75084a728c0a47bf00
mark7dev <mcamacho_ce7@live.com.mx>
2018-08-14 15:24:00 -0500

    version2.1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+import request from 'superagent'; // npm install --save superagent
+
 class App extends Component {
   constructor() {
     super();
 
      this.state = {
+       cities: [],
       show: false
     };
   }
@@ -15,6 +18,29 @@ class App extends Component {
     this.setState({
       show: true
     });
+  }
+
+  addCity = (e) => {
+    //I.If ENTER was pressed
+    const ENTER_KEY = 13;
+
+    if (e.keyCode === ENTER_KEY) {
+      //II. Save new city in 'cities'.
+      this.setState({
+        cities: [
+          //III. Get past data
+          ...this.state.cities,
+          {
+            id: this.state.cities.length + 1,
+            name: e.target.value
+          }
+        ],
+        show: false
+      });
+
+      //IV. Clean the input.
+      e.target.value = '';
+    }
   }
 
   render() {
@@ -28,10 +54,16 @@ class App extends Component {
         <div className='grid'>
           <aside className='app__aside'>
             <h1 className='app__title'>All countries</h1>
-            <a href='#' className='app__country'>France</a>
-    { this.state.show && <input autoFocus type='text' placeholder='Location' className='app__input' /> }
+            { this.state.cities.map(city => {
+              return <a key={ city.id } href='#' className='app__country'>{ city.name }</a>
+            }) }
+            { this.state.show && <input onKeyUp={ this.addCity } autoFocus type='text' placeholder='Location' className='app__input' /> }
           </aside>
-          <section className='app__view'>Text</section>
+          <section className='app__view'>
+            <div>
+              Summary
+            </div>
+          </section>
         </div>
       </div>
     );
